e2ea503004343a9424d93395d02b4809be4bed22
ThushariSenevirathne <108646329+ThushariSenevirathne@users.noreply.github.com>
2024-01-22 11:54:23 +0530

    add all

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/HomeNew';
+import SignIn from './pages/SignInNew';
+import About from './pages/AboutNew';
+import Profile from './pages/ProfileNew';
+import SignUp from './pages/SignUpNew';
 
-export default function App(){
-return <h1 className='text-red-400'>app</h1>
+export default function App(){ 
+    return <BrowserRouter>
+    <Routes>
+        <Route Path='/' element={<Home/>}/>
+        <Route Path='/sign-in' element={<SignIn/>}/>
+        <Route Path='/sign-up' element={<SignUp/>}/>
+        <Route Path='/about' element={<About/>}/>
+        <Route Path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
 }
 
